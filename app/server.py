3a1830ff24720991f2476a427be7ab6ed59ed0a3
Vivek Vaddina <vivek.vaddina@mytaxi.com>
2020-04-12 15:20:39 +0200

    ♻️ adapt fastai template

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://www.googleapis.com/drive/v3/files/1UW_tqH0_MgK0Dimp2bEOMdZMTqT_1ovb?alt=media&key=AIzaSyDbfq9Vg9mFHdZwXL3n9prESF1zFMQG2-Y'
+export_file_name = 'intel_image_classifier.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['buildings', 'forest', 'glacier', 'mountain', 'sea', 'street']
 path = Path(__file__).parent
 
 app = Starlette()
